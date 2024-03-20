b70147987621db146949cf0a27a60db73fc3dd4e
ArthurMDC <arthurmoura427@gmail.com>
2024-03-20 09:37:19 -0300

    ALteração aula

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let a = "5";
 let b = 2;
 console.log(typeof(a));
 console.log(typeof(b));
-*/
 
 //let numero1 = 0.1;
 //let numero2 = 0.2;
@@ -17,11 +16,29 @@ let resultadoTexto = texto1 + ", " + texto2 + "dkaskdaskdak" + "ifjfidsfjsd";
 console.log(resultadoTexto);
 
 
-/*
 let resultado = a - b;
 let resultado2 = a + b;
 
 console.log(resultado);
 console.log(resultado2)
+
+*/
+
+
+let idade = 18;
+let nome= "joao";
+let altura = 1.80;
+
+
+if(idade >= 18 || nome != "pedro") {
+    console.log("ola ")
+}
+
+ /*
+let nome = "joao";
+
+if (nome == "joao") {
+    console.log("Oi, joao!");
+}
 
 */
